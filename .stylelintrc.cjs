9da05ba653d6b81e2158da7ac0d615f7f84396bf
KhanofLegend <43519277+KhanofLegend@users.noreply.github.com>
2025-12-01 16:34:38 +0100

    Updates

---
+++ .stylelintrc.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ["stylelint-config-standard"],
+  rules: {
+    "at-rule-no-unknown": [
+      true,
+      {
+        "ignoreAtRules": [
+          "tailwind",
+          "apply",
+          "variants",
+          "screen",
+          "layer",
+          "responsive",
+          "config"
+        ]
+      }
+    ]
+  }
+};
