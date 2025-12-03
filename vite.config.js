3e1892cec8263294eba864305f3314a023ded5c3
Alicia <114044160+Fern-Ali@users.noreply.github.com>
2025-12-03 13:41:20 -0500

    Update repository name in vite.config.js BACK to romamart.ca

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // Get your repository name from the URL: github.com/username/repository-name
 // *** IMPORTANT: Replace 'roma-mart-site' with the actual name of your GitHub repo ***
-const REPO_NAME = 'web-demo'; 
+const REPO_NAME = 'romamart.ca'; 
 
 export default defineConfig({
   plugins: [
