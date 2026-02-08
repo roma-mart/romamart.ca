4798a0b66104e9c5fdbe8dcf101680ddff126dad
KhanofLegend <KhanofLegend@users.noreply.github.com>
2026-02-08 01:38:29 +0000

    deploy: a5f76d9220621a000ffeea02f7ab6d72794ea512

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * PWA implementation with smart caching strategies
  */
 
-const CACHE_VERSION = /* __CACHE_VERSION__ */ 'roma-mart-be13a4c8';
+const CACHE_VERSION = /* __CACHE_VERSION__ */ 'roma-mart-9eb7fd5a';
 const BASE_URL = '/';
 
 // Assets to cache immediately on install
@@ -19,20 +19,21 @@ const PRECACHE_ASSETS = [
   `${BASE_URL}android/android-launchericon-512-512.png`,
   `${BASE_URL}ios/180.png`,
   `${BASE_URL}windows11/Square150x150Logo.scale-100.png`,
-    `${BASE_URL}assets/AboutPage-Cz3gnqQc.js`,
-  `${BASE_URL}assets/AccessibilityPage-A_n9oh0V.js`,
-  `${BASE_URL}assets/ContactPage-CMUp10GB.js`,
-  `${BASE_URL}assets/CookiesPage-Cd6JFH54.js`,
-  `${BASE_URL}assets/LocationsPage-DltYZlFi.js`,
-  `${BASE_URL}assets/PrivacyPage-B0hZldM5.js`,
-  `${BASE_URL}assets/ReturnPolicyPage-uOGLEJLO.js`,
-  `${BASE_URL}assets/RoCafePage-Dh3QjlAv.js`,
-  `${BASE_URL}assets/ServicesPage-ZXit5hJb.js`,
-  `${BASE_URL}assets/TermsPage-BDQgX3BE.js`,
-  `${BASE_URL}assets/icons-DfzCwffX.js`,
-  `${BASE_URL}assets/index-DG83lBQG.css`,
-  `${BASE_URL}assets/index-OcroJgM8.js`,
-  `${BASE_URL}assets/motion-C6zIQYda.js`,
+    `${BASE_URL}assets/AboutPage-BPmCmWUS.js`,
+  `${BASE_URL}assets/AccessibilityPage-Ba9ZU1kd.js`,
+  `${BASE_URL}assets/ContactPage-B6QOkYaO.js`,
+  `${BASE_URL}assets/CookiesPage-Zq-Flo0_.js`,
+  `${BASE_URL}assets/ImageCarousel-1OjiOLIS.js`,
+  `${BASE_URL}assets/LocationsPage-C94PP1nr.js`,
+  `${BASE_URL}assets/PrivacyPage-D0jNkrGF.js`,
+  `${BASE_URL}assets/ReturnPolicyPage-CFvfYNEo.js`,
+  `${BASE_URL}assets/RoCafePage-BVlXXkki.js`,
+  `${BASE_URL}assets/ServicesPage-GZOA76FI.js`,
+  `${BASE_URL}assets/TermsPage-D6sPAL1k.js`,
+  `${BASE_URL}assets/icons-CbuKbt0t.js`,
+  `${BASE_URL}assets/index-D_1gXnET.css`,
+  `${BASE_URL}assets/index-eD9prxg_.js`,
+  `${BASE_URL}assets/motion-CsacCTTH.js`,
   `${BASE_URL}assets/phone-DvfqJ5kM.js`,
   `${BASE_URL}assets/react-vendor-labSKdyf.js`
 ];
